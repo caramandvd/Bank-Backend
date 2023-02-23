17674f4b0fbecf1830b4631458e4735672747173
David Caraman <caramandvd@gmail.com>
2023-02-24 01:13:46 +0200

    I have created the users_credentials_repository.py. I added a hashing method in the create function.

---
+++ Model/Repository/users_credentials_repository.py
@@ -0,0 +1,41 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from Model.Domain.users_credentials import UsersCredentials
+from Utils.utils import Base
+
+
+class UsersCredentialsRepository:
+    def __init__(self):
+        self.session = sessionmaker(create_engine('mysql+pymysql://root@localhost:3306/bankdashboarddb'))()
+
+    def create(self, user_id, username, user_password):
+
+        def hash_password(password):
+            import hashlib
+            return hashlib.sha256(password.encode('UTF-8')).hexdigest()
+
+        new_user_credentials = UsersCredentials(
+            user_id=user_id,
+            username=username,
+            user_password_hash=hash_password(user_password)
+        )
+        self.session.add(new_user_credentials)
+        self.session.commit()
+
+    def read(self, user_id):
+        return self.session.query(UsersCredentials).filter_by(user_id=user_id).first()
+
+    def update(self, user_id, password):
+        self.session.query(UsersCredentials).filter_by(user_id=user_id).update(password)
+        self.session.commit()
+
+    def delete(self, user_id):
+        self.session.query(UsersCredentials).filter_by(user_id=user_id).delete()
+        self.session.commit()
+
+
+if __name__ == '__main__':
+    Base.metadata.create_all(create_engine('mysql+pymysql://root@localhost:3306/bankdashboarddb'))
+    repo = UsersCredentialsRepository()
+    repo.create('5030329082416', 'caramandvd', 'password')
+    
