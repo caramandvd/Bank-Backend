d538d50fec31d79a80a811af3f4eb045af3a7f95
David Caraman <caramandvd@gmail.com>
2023-02-24 01:20:23 +0200

    I have added 'try: engine.connect() print("Success") except SQLAlchemyError as err: print("Error", err.__cause__)'

--- Utils/utils.py
+++ Utils/utils.py
@@ -1,8 +1,15 @@
 from sqlalchemy import create_engine
+from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 engine = create_engine('mysql+pymysql://root@localhost:3306/bankdashboarddb')
+
+try:
+    engine.connect()
+    print("Success")
+except SQLAlchemyError as err:
+    print("Error", err.__cause__)
 
 if __name__ == '__main__':
     engine = create_engine('mysql+pymysql://root@localhost:3306/bankdashboarddb')
