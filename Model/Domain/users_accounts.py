c081c909613d72320474d1b4e47f9284fc8fcdcb
David Caraman <caramandvd@gmail.com>
2023-02-24 01:29:22 +0200

    Added foreign_key for currencies.currency and I deleted the unused imports

--- Model/Domain/users_accounts.py
+++ Model/Domain/users_accounts.py
@@ -1,5 +1,5 @@
 from Utils.utils import Base
-from sqlalchemy import Column, String, Integer, Date, Float, ForeignKey
+from sqlalchemy import Column, String, Float, ForeignKey
 
 
 class UsersAccounts(Base):
@@ -7,9 +7,8 @@ class UsersAccounts(Base):
     __tablename__ = "users_accounts"
     user_id = Column(String(13), foreign_key=ForeignKey('users.user_id', ondelete="CASCADE"))
     account_number = Column(String(24), primary_key=True, foreign_key=ForeignKey('users_accounts.account_number', ondelete="CASCADE"))
-    currency = Column(String(3))
+    currency = Column(String(3), foreign_key=ForeignKey('currencies.currency', ondelete="CASCADE"))
     amount = Column(Float(2))
-
 
     def __repr__(self):
         return f'{self.user_id}; {self.account_number}; {self.currency}; {self.amount}'
