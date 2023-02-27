22fd3a76d566069a91126ae0f71aec187319eb9d
David Caraman <caramandvd@gmail.com>
2023-02-27 23:28:00 +0200

    refactor: added relationship key

--- Model/Domain/currencies.py
+++ Model/Domain/currencies.py
@@ -1,3 +1,4 @@
+from sqlalchemy.orm import relationship
 from Utils.utils import Base
 from sqlalchemy import Column, String
 
@@ -5,6 +6,8 @@ from sqlalchemy import Column, String
 class Currencies(Base):
     __tablename__ = "currencies"
     currency = Column(String(3), primary_key=True)
+    from_currency = relationship("ExchangeRates", backref="currencies")
+    to_currency = relationship("ExchangeRates", backref="currencies")
 
     def __str__(self):
         return f'{self.currency}'
